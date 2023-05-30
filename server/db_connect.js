f23e5a35fca7c34581424e3bebf10c2f2d802525
khoob-chand <khoobchandjhariya48@gmail.com>
2023-05-30 18:03:35 +0530

    awesome

--- server/db_connect.js
+++ server/db_connect.js
@@ -1,14 +1,22 @@
 const mongoose=require('mongoose');
-    mongoose.connect('mongodb://127.0.0.1:27017/test_db',{
-        useNewUrlParser: true,
-        // useFindAndModify: false,
-        useUnifiedTopology: true
-    });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
+const Db="mongodb+srv://khoobchandjhariya48:khoob@cluster0.dqzahfd.mongodb.net/?retryWrites=true&w=majority";
+    // mongoose.connect('mongodb://127.0.0.1:27017/test_db',{
+    //     useNewUrlParser: true,
+    //     // useFindAndModify: false,
+    //     useUnifiedTopology: true
+    // });
+    mongoose.connect(Db,{ useNewUrlParser: true,
+          // useFindAndModify: false,
+          useUnifiedTopology: true}).then(()=>{
+      console.log("Connected successfully khoob");
+    }).catch((err)=>{
+      console.log(err)});
+    
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+
+// });
 const UserSchema = new mongoose.Schema({
     name: {
       type: String,
